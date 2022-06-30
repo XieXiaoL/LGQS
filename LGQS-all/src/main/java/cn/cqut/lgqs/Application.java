2d23d9d4ff9c31d3533be746f49cd9139298d375
XieXiaoL <1256570764@qq.com>
2022-06-30 19:22:29 +0800

    LGQS-管理后台轮播图管理功能

--- LGQS-all/src/main/java/cn/cqut/lgqs/Application.java
+++ LGQS-all/src/main/java/cn/cqut/lgqs/Application.java
@@ -28,6 +28,7 @@ public class Application {
         System.out.println("6.20  微信前台订单操作功能");
         System.out.println("6.25  管理后台订单管理功能");
         System.out.println("6.20  微信前台反馈功能");
+        System.out.println("6.30  管理后台轮播图管理功能");
         SpringApplication.run(Application.class, args);
     }
 
